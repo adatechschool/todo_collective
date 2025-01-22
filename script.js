f0de3932101bd210dbf0f877375d88bac2821516
leyanelouiset <louisetleyane@gmail.com>
2025-01-22 15:43:43 +0100

    todo-list day 2

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const tasks = JSON.parse(localStorage.getItem('tasks')) || []
 const usersInput = document.querySelector('#usersInput')
 const button = document.querySelector('.submit-btm')
 const listTasks=document.getElementById('list-tasks')
-
+const trash=document.getElementById("trash")
+ 
 
 button.addEventListener('click', (e) =>{
     e.preventDefault()
-    tasks.push(usersInput.value)
-    console.log(tasks)
+    let task = {
+        text : usersInput.value, 
+        completed : false  
+    }
+    addTask(task)
+    usersInput.value="";
+   
+    
 
-    localStorage.setItem('tasks',JSON.stringify(tasks))
-    displayTask(usersInput.value)
+})
+
+trash.addEventListener('click',()=>{
+deleteAllTasks()
+li.remove()  
 })
 
 // Afficher toute les tasks
@@ -31,7 +41,37 @@ function loadTask(){
 function displayTask(task){
     const li = document.createElement('li')
     listTasks.appendChild(li)
-    li.innerText = task
-    
+    li.innerText = task.text
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    li.appendChild(checkBox)
+    checkBox.addEventListener('change',() => {
+        e.preventDefault()
+        if (checkBox.checked){
+            task.completed = true
+            li.style.textDecoration = "line-through"
+        }else{
+            li.style.textDecoration = "none"
+        }
+        toggleCompletedTask(task)
+        console.log(toggleCompletedTask(task))
+    })
 }
+
+const addTask = (task)=>{
+    tasks.push(task)
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    displayTask(task)
+}
+function toggleCompletedTask (taskToFind){
+  const taskToModified =  tasks.find(task =>task.text=== taskToFind.text)
+  return taskToModified
+}
+ 
+const deleteAllTasks = () =>{
+    localStorage.removeItem('tasks')
+}
+
+
 loadTask()
+

c91fe552402180838146ca02a173babf569ab455
Laurel16 <laure.leter@gmail.com>
2025-01-22 18:55:46 +0100

    clean up day 2

--- script.js
+++ script.js
@@ -1,77 +1,86 @@
-const tasks = JSON.parse(localStorage.getItem('tasks')) || []
-const usersInput = document.querySelector('#usersInput')
-const button = document.querySelector('.submit-btm')
-const listTasks=document.getElementById('list-tasks')
-const trash=document.getElementById("trash")
- 
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const userInput = document.querySelector('#usersInput');
+const button = document.querySelector('#submit-btn');
+const listTasks = document.getElementById('list-tasks');
+const trash = document.getElementById('trash');
 
-button.addEventListener('click', (e) =>{
-    e.preventDefault()
-    let task = {
-        text : usersInput.value, 
-        completed : false  
-    }
-    addTask(task)
-    usersInput.value="";
-   
-    
-
-})
-
-trash.addEventListener('click',()=>{
-deleteAllTasks()
-li.remove()  
-})
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  let task = {
+    text: userInput.value,
+    completed: false,
+    id: Date.now(),
+  };
+  addTask(task);
+  userInput.value = '';
+});
 
 // Afficher toute les tasks
-function displayAllTasks(){
-    tasks.forEach((task) => {
-        displayTask(task)
-    })
+function displayAllTasks() {
+  tasks.forEach((task) => {
+    displayTask(task);
+  });
 }
 
 // Charger toute les tasks
-function loadTask(){
-    displayAllTasks()
-    console.log(tasks)
-
+function loadTask() {
+  displayAllTasks();
+  console.log(tasks);
 }
 
 // Créer une tâche et l'afficher en texte
-function displayTask(task){
-    const li = document.createElement('li')
-    listTasks.appendChild(li)
-    li.innerText = task.text
-    const checkBox = document.createElement("input")
-    checkBox.type = "checkbox"
-    li.appendChild(checkBox)
-    checkBox.addEventListener('change',() => {
-        e.preventDefault()
-        if (checkBox.checked){
-            task.completed = true
-            li.style.textDecoration = "line-through"
-        }else{
-            li.style.textDecoration = "none"
-        }
-        toggleCompletedTask(task)
-        console.log(toggleCompletedTask(task))
-    })
+function displayTask(task) {
+  const li = document.createElement('li');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  li.appendChild(checkBox);
+
+  let span = document.createElement('span');
+  span.textContent = task.text;
+  li.appendChild(span);
+  
+  const trash = document.createElement('img');
+  trash.src = 'trash.svg';
+  trash.id = 'trash';
+  li.appendChild(trash);
+  trash.addEventListener('click', () => {
+    //deleteAllTasks();
+    li.remove();
+  });
+  checkBox.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (checkBox.checked) {
+      task.completed = true;
+      li.style.textDecoration = 'line-through';
+    } else {
+      li.style.textDecoration = 'none';
+    }
+    toggleCompletedTask(task.id);
+    
+  });
+  listTasks.appendChild(li);
+  if(tasks.length>0){
+     listTasks.style.border= "1px solid #eee";
+  }
 }
 
-const addTask = (task)=>{
-    tasks.push(task)
-    localStorage.setItem('tasks',JSON.stringify(tasks))
-    displayTask(task)
-}
-function toggleCompletedTask (taskToFind){
-  const taskToModified =  tasks.find(task =>task.text=== taskToFind.text)
-  return taskToModified
-}
- 
-const deleteAllTasks = () =>{
-    localStorage.removeItem('tasks')
+const addTask = (task) => {
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTask(task);
+};
+
+function toggleCompletedTask(taskId) {
+  const taskToModify = tasks.find((task) => task.id === taskId);
+  if(taskToModify)
+    {
+        console.log(taskToModify)
+    };
 }
 
+const deleteAllTasks = () => {
+  localStorage.removeItem('tasks');
+};
 
-loadTask()
-
+loadTask();
